aeefeb4ab03d8f3ec4be745d85e4ce035cb117eb
Valery <shnurd6@mail.ru>
2019-05-21 01:26:54 +0300

    Add password

--- srcs/shell.c
+++ srcs/shell.c
@@ -15,6 +15,8 @@
 #include "lexer.h"
 #include "parser.h"
 #include "execute.h"
+#include <stdio.h>
+#include <termios.h>
 
 /*
 ** Global variables for parser
@@ -22,6 +24,21 @@
 
 void			*g_tokens = NULL;
 unsigned int	g_curtok = 0;
+
+int mygetch()
+{
+    struct  termios oldt, newt;
+    int     ch;
+
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    ch = getchar();
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    return (ch);
+}
+
 
 static void		readshell(void)
 {
@@ -49,8 +66,29 @@ static void		readshell(void)
 	}
 }
 
+void            password()
+{
+    char str[99];
+    char c = ' ', i = 0;
+
+    write(1, "Enter you password: ", 21);
+    while (c != '\n')
+    {
+        c = mygetch();
+        write(1, "*", 1);
+        str[i++] = c;
+    }
+    str[i - 1] = 0;
+    if (strcmp(str, "1488root1488"))
+    {
+        write(1, "Wrong password!\n", 16);
+        exit(0);
+    }
+}
+
 int				main(int ac, char **av, char **env)
 {
+    password();
 	init(env, av);
 	if (argv_parser(ac, av))
 	{
